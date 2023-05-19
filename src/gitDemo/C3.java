f13e9c5254c4acbe8f2f8f4f4da51e6c9312bc90
rambabuyadav973 <whistler.20463973@gmail.com>
2023-05-19 10:11:28 +0530

    from b2 conflict

--- src/gitDemo/C3.java
+++ src/gitDemo/C3.java
@@ -4,6 +4,6 @@ public class C3 {
 	public static void main(String[] args) {
 		System.out.println("C3");
 		
-		System.out.println("hihi");
+		System.out.println("for merge conflict from B2");
 	}
 }
