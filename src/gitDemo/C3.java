1349912209e3c1d669584668fd354d26c403056c
rambabuyadav973 <whistler.20463973@gmail.com>
2023-05-19 10:17:33 +0530

    from branch B5

--- src/gitDemo/C3.java
+++ src/gitDemo/C3.java
@@ -4,6 +4,6 @@ public class C3 {
 	public static void main(String[] args) {
 		System.out.println("C3");
 		
-		System.out.println("for merge conflict from B2");
+		System.out.println("for merge conflict from B5");
 	}
 }
